ae5c1ea42c2f2c9751cc523c81e50fd31946f185
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-08-04 23:05:57 +0200

    fix register usage

--- src/interface.rs
+++ src/interface.rs
@@ -8,7 +8,7 @@
 //! # Low-Level UART interface implementation
 //! 
 
-use ruspiro_register::define_registers;
+use ruspiro_register::{define_registers, RegisterFieldValue};
 use ruspiro_gpio::GPIO;
 
 // initialize the UART0 peripheral of the Raspberry Pi3. This will reserve 2 GPIO pins for UART0 usage.
@@ -22,16 +22,22 @@ pub(crate) fn uart0_init(clock_rate: u32, baud_rate: u32) -> Result<(), &'static
         // returns OK only if both pins could be setup correctly
         maybe_tx.and(maybe_ty)
     }).map(|_| {
-        AUX_ENABLES::Register.set(0x1); // enable mini UART
+        AUX_ENABLES::Register.write(AUX_ENABLES::MINIUART_ENABLE, 0x1); // enable mini UART
         AUX_MU_IER_REG::Register.set(0x0); // disable interrupts
         AUX_MU_CNTL_REG::Register.set(0x0); // disable transmitter (to set new baud rate)
-        AUX_MU_LCR_REG::Register.set(0x3); // set 8bit data transfer mode
+        AUX_MU_LCR_REG::Register.write(AUX_MU_LCR_REG::DATASIZE, 0x3); // set 8bit data transfer mode
         AUX_MU_MCR_REG::Register.set(0x0); // set UART_RTS line to high (ready to send)
         AUX_MU_IER_REG::Register.set(0x0); // disable interrupts
-        AUX_MU_IIR_REG::Register.set(0xC6); // clear recieve/transmit FIFO, set FIFO as always enabled
+        AUX_MU_IIR_REG::Register.write_value(
+            RegisterFieldValue::<u32>::new(AUX_MU_IIR_REG::IRQID_FIFOCLR, 0b11) |
+            RegisterFieldValue::<u32>::new(AUX_MU_IIR_REG::FIFO_ENABLES, 0b11)
+        ); // clear recieve/transmit FIFO, set FIFO as always enabled
         AUX_MU_BAUD_REG::Register.set(clock_rate/(8*baud_rate)-1); // set the baud rate based on the core clock rate
 
-        AUX_MU_CNTL_REG::Register.set(0x3); // enable transmitter
+        AUX_MU_CNTL_REG::Register.write_value(
+            RegisterFieldValue::<u32>::new(AUX_MU_CNTL_REG::RCV_ENABLE, 0x1) |
+            RegisterFieldValue::<u32>::new(AUX_MU_CNTL_REG::TRANS_ENABLE, 0x1)
+        ); // enable receiver and transmitter
     })
 }
 
@@ -67,21 +73,42 @@ use super::UART0_BASE as UART0_BASE;
 
 // specify the UART0 registers
 define_registers! [
-    AUX_IRQ:          ReadWrite<u32> @ UART0_BASE + 0x00 => [],
-    AUX_ENABLES:      ReadWrite<u32> @ UART0_BASE + 0x04 => [],
-    AUX_MU_IO_REG:    ReadWrite<u32> @ UART0_BASE + 0x40 => [],
-    AUX_MU_IER_REG:   ReadWrite<u32> @ UART0_BASE + 0x44 => [],
-    AUX_MU_IIR_REG:   ReadWrite<u32> @ UART0_BASE + 0x48 => [],
-    AUX_MU_LCR_REG:   ReadWrite<u32> @ UART0_BASE + 0x4C => [],
-    AUX_MU_MCR_REG:   ReadWrite<u32> @ UART0_BASE + 0x50 => [],
+    AUX_IRQ:          ReadWrite<u32> @ UART0_BASE + 0x00,
+    AUX_ENABLES:      ReadWrite<u32> @ UART0_BASE + 0x04 => [
+        MINIUART_ENABLE OFFSET(0),
+        SPI1_ENABLE OFFSET(1),
+        SPI2_ENABLE OFFSET(2)
+    ],
+    AUX_MU_IO_REG:    ReadWrite<u32> @ UART0_BASE + 0x40,
+    AUX_MU_IER_REG:   ReadWrite<u32> @ UART0_BASE + 0x44,
+    AUX_MU_IIR_REG:   ReadWrite<u32> @ UART0_BASE + 0x48 => [
+        IRQPENDING OFFSET(0),
+        IRQID_FIFOCLR OFFSET(1) BITS(2),
+        FIFO_ENABLES OFFSET(6) BITS(2)
+    ],
+    AUX_MU_LCR_REG:   ReadWrite<u32> @ UART0_BASE + 0x4C => [
+        DATASIZE OFFSET(0) BITS(2),
+        BREAK OFFSET(6),
+        DLAB OFFSET(7)
+    ],
+    AUX_MU_MCR_REG:   ReadWrite<u32> @ UART0_BASE + 0x50,
     AUX_MU_LSR_REG:   ReadOnly<u32> @ UART0_BASE + 0x54 => [
         DATAREADY  OFFSET(0) BITS(0),
         RCVOVERRUN OFFSET(1) BITS(1),
         TRANSEMPTY OFFSET(5) BITS(1),
         TRANSIDLE  OFFSET(6) BITS(1)
     ],
-    AUX_MU_MSR_REG:   ReadWrite<u32> @ UART0_BASE + 0x58 => [],
-    AUX_MU_CNTL_REG:  ReadWrite<u32> @ UART0_BASE + 0x60 => [],
-    AUX_MU_STAT_REG:  ReadWrite<u32> @ UART0_BASE + 0x64 => [],
-    AUX_MU_BAUD_REG:  ReadWrite<u32> @ UART0_BASE + 0x68 => []
+    AUX_MU_MSR_REG:   ReadWrite<u32> @ UART0_BASE + 0x58,
+    AUX_MU_CNTL_REG:  ReadWrite<u32> @ UART0_BASE + 0x60 => [
+        RCV_ENABLE OFFSET(0),
+        TRANS_ENABLE OFFSET(1),
+        AUTO_FLOW_RTS OFFSET(2),
+        AUTO_FLOW_CTS OFFSET(3),
+        AUTO_RTS_LEVEL OFFSET(4) BITS(2),
+        RTS_ASSERT OFFSET(6),
+        CTS_ASSERT OFFSET(7)
+
+    ],
+    AUX_MU_STAT_REG:  ReadWrite<u32> @ UART0_BASE + 0x64,
+    AUX_MU_BAUD_REG:  ReadWrite<u32> @ UART0_BASE + 0x68
 ];
