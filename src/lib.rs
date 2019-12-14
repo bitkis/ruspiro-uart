d3a33be526ab7cb5b648b5c377ee92bd63bd5f9e
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-12-14 17:18:09 +0100

    enable aarch64 build target architecture

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,42 @@
-/*********************************************************************************************************************** 
+/***********************************************************************************************************************
  * Copyright (c) 2019 by the authors
- * 
- * Author: André Borrmann 
+ *
+ * Author: André Borrmann
  * License: Apache License 2.0
  **********************************************************************************************************************/
-#![doc(html_root_url = "https://docs.rs/ruspiro-uart/0.2.0")]
+#![doc(html_root_url = "https://docs.rs/ruspiro-uart/0.3.0")]
 #![no_std]
 #![feature(asm)]
 //! # UART API for Raspberry Pi
-//! 
+//!
 //! This crate provides access to the Uart0 (PL011) and the Uart1 (miniUART) peripheral of the Raspberry Pi. It is quite
 //! helpful during bare metal development to use a terminal console connected to the miniUART of the Raspberry Pi to get
 //! some debug information printed while the program is executed on the device. Especialy if the program is in a state
 //! where there is no other output option or blinking LEDs are not sufficient.
-//! 
+//!
 //! # Example
-//! 
+//!
 //! The proposed usage of the UART is to attach it to a generic console as an output channel instead of using it directly.
 //! To do so, please refer to the [``ruspiro-console`` crate](https://crates.io/crates/ruspiro-console).
-//! 
+//!
 //! But in case you would like to use the uart without the console abstraction it is recommended to wrap it into a singleton
 //! to guaranty safe cross core access and ensure only one time initialization. In the example we pass a fixed core clock rate to
 //! the initialization function. However, the real core clock rate could be optained with a call to the mailbox property
 //! tag interface of the Raspberry Pi (see [``ruspiro-mailbox`` crate](https://crates.io/crates/ruspiro-mailbox) for details.).
-//! 
-//! ```
+//!
+//! ```no_run
 //! use ruspiro_singleton::Singleton; // don't forget the dependency to be setup in ``Cargo.toml``
 //! use ruspiro_uart::Uart1;
-//! 
+//!
 //! static UART: Singleton<Uart1> = Singleton::new(Uart1::new());
-//! 
-//! fn demo() {
-//!     let _ = UART.take_for(|uart| uart.initialize(250_000_000, 115_200)); // initialize(...) gives a Result, you may want to panic if there is an Error returned.
-//! 
+//!
+//! fn main() {
+//!     let _ = UART.take_for(|uart| uart.initialize(250_000_000, 115_200));
+//!     // initialize(...) gives a [Result], you may want to panic if there is an Error returned.
+//!
 //!     print_something("Hello Uart...");
 //! }
-//! 
+//!
 //! fn print_something(s: &str) {
 //!     UART.take_for(|uart| uart.send_string(s));
 //! }
--- src/uart1/mod.rs
+++ src/uart1/mod.rs
@@ -1,26 +1,26 @@
-/*********************************************************************************************************************** 
+/***********************************************************************************************************************
  * Copyright (c) 2019 by the authors
- * 
- * Author: André Borrmann 
+ *
+ * Author: André Borrmann
  * License: Apache License 2.0
  **********************************************************************************************************************/
 
 //! # Uart1 (miniUart) API
-//! 
+//!
 //! As per the Raspberry Pi peripheral document the miniUART is a lightweight serial communication channel that does only
 //! need 3 wires (TX, RX, GND) to be connected to the device. The miniUART is typically used to connect the device to
-//! a PC or Mac that runs a terminal console application and is able to display the characters received through this 
+//! a PC or Mac that runs a terminal console application and is able to display the characters received through this
 //! channel. This allows to pass debug information from the device running the bare metal kernel to improve root cause
 //! analysis.
-//! 
+//!
 //! There is no singleton accessor provided for this peripheral as it will be quite likely attached to a ``Console``
 //! abstraction that will than **own** this peripheral and should itself providing exclusive access to the inner accessor
 //! of the actual device. Please refer to the [``ruspiro-console`` crate](https://crates.io/crates/ruspiro-console).
-//! 
+//!
 
 extern crate alloc;
-use ruspiro_console::ConsoleImpl;
 use crate::InterruptType;
+use ruspiro_console::ConsoleImpl;
 
 mod interface;
 
@@ -32,40 +32,44 @@ pub struct Uart1 {
 impl Uart1 {
     /// Get a new Uart1 instance, that needs to be initialized before it can be used.
     /// # Example
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
     /// let _miniUart = Uart1::new();
     /// # }
+    /// ```
     pub const fn new() -> Self {
-        Uart1 {
-            initialized: false,
-        }
+        Uart1 { initialized: false }
     }
 
     /// Initialize the Uart1 peripheral for usage. It takes the core clock rate and the
     /// baud rate to configure correct communication speed.
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
     /// let mut uart = Uart1::new();
     /// assert_eq!(uart.initialize(250_000_000, 115_200), Ok(()));
     /// # }
     /// ```
-    /// 
+    ///
     pub fn initialize(&mut self, clock_rate: u32, baud_rate: u32) -> Result<(), &'static str> {
-        interface::uart1_init(clock_rate, baud_rate)
-            .map(|_| { self.initialized = true; } )
+        interface::uart1_init(clock_rate, baud_rate).map(|_| {
+            self.initialized = true;
+        })
     }
 
     /// Send a single character to the uart peripheral
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
     /// # let mut uart = Uart1::new();
     /// # let _ = uart.initialize(250_000_000, 115_200);
     /// uart.send_char('A');
     /// # }
     /// ```
-    /// 
+    ///
     pub fn send_char(&self, c: char) {
         if self.initialized {
             interface::uart1_send_char(c);
@@ -74,14 +78,15 @@ impl Uart1 {
 
     /// Send a string to the uart peripheral
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
     /// # let mut uart = Uart1::new();
     /// # let _ = uart.initialize(250_000_000, 115_200);
     /// uart.send_string("Test string with line break\r\n");
     /// # }
     /// ```
-    /// 
+    ///
     pub fn send_string(&self, s: &str) {
         if self.initialized {
             interface::uart1_send_string(s);
@@ -90,7 +95,8 @@ impl Uart1 {
 
     /// Send a byte buffer to the uart peripheral
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
     /// # let mut uart = Uart1::new();
     /// # let _ = uart.initialize(20_000_000, 115_200);
@@ -105,7 +111,8 @@ impl Uart1 {
 
     /// convert a given u64 into it's hex representation and send to uart
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
     /// # let mut uart = Uart1::new();
     /// # let _ = uart.initialize(20_000_000, 115_200);
@@ -115,42 +122,43 @@ impl Uart1 {
     pub fn send_hex(&self, value: u64) {
         if value == 0 {
             self.send_string("0x0");
-            return;    
+            return;
         }
-        const HEXCHAR : &[u8] = "0123456789ABCDEF".as_bytes();
+        const HEXCHAR: &[u8] = b"0123456789ABCDEF";
         let mut tmp = value;
-        let mut hex: [u8;16] = [0; 16];
+        let mut hex: [u8; 16] = [0; 16];
         let mut idx = 0;
         while tmp != 0 {
             hex[idx] = HEXCHAR[(tmp & 0xF) as usize];
-            tmp = tmp >> 4;
-            idx = idx+1;
+            tmp >>= 4;
+            idx += 1;
         }
-        
+
         self.send_string("0x");
         for i in 0..16 {
-            if hex[15-i] != 0 {
-                self.send_char(hex[15-i] as char);
+            if hex[15 - i] != 0 {
+                self.send_char(hex[15 - i] as char);
             }
-        }    
+        }
     }
 
     /// Try to recieve data from the Uart of the given size
     /// If the requested size could be read it returns a ``Ok(data: Vec<u8>)`` containing the data
     /// otherwise an ``Err(msg: &str)``.
-    /// 
+    ///
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
-    /// # let uart = Uart1::new();
+    /// # let mut uart = Uart1::new();
     /// # let _ = uart.initialize(250_000_000, 115_200);
-    /// let data = uart.try_receive_data(8).expect("unable to receive 8 bytes");
+    /// let mut buffer: [u8; 8] = [0; 8];
+    /// let rx_size = uart.try_receive_data(&mut buffer).expect("unable to receive data");
     /// # }
     /// ```
-    /// 
     pub fn try_receive_data(&self, buffer: &mut [u8]) -> Result<usize, &'static str> {
         if self.initialized {
-            if buffer.len() < 1 {
+            if buffer.is_empty() {
                 Err("buffer size expected to be at least 1")
             } else {
                 for c in 0..buffer.len() {
@@ -164,9 +172,24 @@ impl Uart1 {
         }
     }
 
+    /// Recieve data from the Uart of the given size, blocking the current execution until the
+    /// requested amount if data has been received.
+    /// If the requested size could be read it returns a ``Ok(size: usize)`` containing the data
+    /// otherwise an ``Err(msg: &str)``.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
+    /// # fn doc() {
+    /// # let mut uart = Uart1::new();
+    /// # let _ = uart.initialize(250_000_000, 115_200);
+    /// let mut buffer: [u8; 8] = [0; 8];
+    /// let rx_size = uart.receive_data(&mut buffer).expect("unable to receive data");
+    /// # }
+    /// ```
     pub fn receive_data(&self, buffer: &mut [u8]) -> Result<usize, &'static str> {
         if self.initialized {
-            if buffer.len() < 1 {
+            if buffer.is_empty() {
                 Err("buffer size expected to be at least 1")
             } else {
                 for c in 0..buffer.len() {
@@ -184,13 +207,16 @@ impl Uart1 {
     /// that shall be triggered. To receive/handle the interrupts a corresponding interrupt handler need to be
     /// implemented, for example by using the [``ruspiro-interrupt`` crate](https://crates.io/crates/ruspiro-interrupt).
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::InterruptType;
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
-    /// # let uart = Uart1::new();
+    /// # let mut uart = Uart1::new();
     /// # let _ = uart.initialize(250_000_000, 115_200);
     /// // enable the interrupt to be triggered when data is recieved by the miniUart
     /// uart.enable_interrupts(InterruptType::Receive);
     /// # }
+    /// ```
     pub fn enable_interrupts(&self, i_type: InterruptType) {
         if self.initialized {
             interface::uart1_enable_interrupts(i_type);
@@ -200,13 +226,16 @@ impl Uart1 {
     /// Disable Interrupts from beeing triggered by the miniUart. The ``i_type`` specifies the interrupts
     /// that shall disbabled.
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::InterruptType;
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
-    /// # let uart = Uart1::new();
+    /// # let mut uart = Uart1::new();
     /// # let _ = uart.initialize(250_000_000, 115_200);
     /// // disable the interrupt to be triggered when data is recieved by the miniUart
     /// uart.disable_interrupts(InterruptType::Receive);
     /// # }
+    /// ```
     pub fn disable_interrupts(&self, i_type: InterruptType) {
         if self.initialized {
             interface::uart1_disable_interrupts(i_type);
@@ -218,15 +247,17 @@ impl Uart1 {
     /// Bit [1:2] -> 01 = transmit register is empty
     ///              10 = recieve register holds valid data
     /// # Example
-    /// ```
+    /// ```no_run
+    /// # use ruspiro_uart::uart1::*;
     /// # fn doc() {
-    /// # let uart = Uart1::new();
+    /// # let mut uart = Uart1::new();
     /// # let _ = uart.initialize(250_000_000, 115_200);
     /// let irq_status = uart.get_interrupt_status();
     /// if (irq_status & 0b010) != 0 {
     ///     println!("transmit register empty raised");
     /// }
     /// # }
+    /// ```
     pub fn get_interrupt_status(&self) -> u32 {
         if self.initialized {
             interface::uart1_get_interrupt_status()
